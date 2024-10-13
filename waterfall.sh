07f246a893f78172b9fd84afccf43cd2d89df5e1
mohawktayler2 <120407680+mohawktayler2@users.noreply.github.com>
2024-10-12 18:30:18 -0600

    Update waterfall.sh

--- waterfall.sh
+++ waterfall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd waterfall
-wget -O waterfall.jar https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/562/downloads/waterfall-1.20-562.jar
+wget -O velocity.jar https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/562/downloads/waterfall-1.20-562.jar
 while [ true ]; do
-    java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar waterfall.jar --nogui
+    java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
 
     echo Server restarting...
     echo Press CTRL + C to stop.
