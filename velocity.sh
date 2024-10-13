6c7242dd7238533a994ebd3e1f60a067918b084d
mohawktayler2 <120407680+mohawktayler2@users.noreply.github.com>
2024-10-13 20:18:10 +0000

    modified:   install.sh 	new file:   locations.yml 	modified:   paper.sh 	new file:   server/.console_history 	new file:   server/banned-ips.json 	new file:   server/banned-players.json 	modified:   server/bukkit.yml 	new file:   server/cache/mojang_1.21.1.jar 	new file:   server/commands.yml 	new file:   server/config/paper-global.yml 	new file:   server/config/paper-world-defaults.yml 	new file:   server/help.yml 	new file:   server/libraries/com/destroystokyo/paper/paper-mojangapi/1.21.1-R0.1-SNAPSHOT/paper-mojangapi-1.21.1-R0.1-SNAPSHOT.jar 	new file:   server/libraries/com/github/carleslc/Simple-YAML/Simple-Configuration/1.8.4/Simple-Configuration-1.8.4.jar 	new file:   server/libraries/com/github/carleslc/Simple-YAML/Simple-Yaml/1.8.4/Simple-Yaml-1.8.4.jar 	new file:   server/libraries/com/github/oshi/oshi-core/6.4.10/oshi-core-6.4.10.jar 	new file:   server/libraries/com/google/code/findbugs/jsr305/3.0.2/jsr305-3.0.2.jar 	new file:   server/libraries/com/google/code/gson/gson/2.10.1/gson-2.10.1.jar

--- velocity.sh
+++ velocity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd waterfall
-wget -O velocity.jar https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/562/downloads/waterfall-1.20-562.jar
+cd velocity
+wget -O velocity.jar https://api.papermc.io/v2/projects/velocity/versions/3.3.0-SNAPSHOT/builds/436/downloads/velocity-3.3.0-SNAPSHOT-436.jar
 while [ true ]; do
     java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
 
